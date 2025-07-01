d28b791ccfcf2bcfa48535aac866d97db61c7ee2
Sudip Debnath <developer.sudip07@gmail.com>
2025-07-01 18:11:35 +0530

    updated style

--- js/custom.js
+++ js/custom.js
@@ -21,3 +21,28 @@ function showAllSkills() {
 function closeSkillsPopup() {
     document.getElementById("allSkillsPopup").classList.remove("active");
 }
+
+/*const popup = document.querySelector('.popup-content');
+let isDragging = false;
+let offsetX = 0;
+let offsetY = 0;
+
+popup.addEventListener('mousedown', function (e) {
+  isDragging = true;
+  offsetX = e.clientX - popup.offsetLeft;
+  offsetY = e.clientY - popup.offsetTop;
+  popup.style.position = 'absolute';
+  popup.style.margin = 0; // Remove margin for free movement
+  popup.style.zIndex = 10000;
+});
+
+document.addEventListener('mouseup', function () {
+  isDragging = false;
+});
+
+document.addEventListener('mousemove', function (e) {
+  if (isDragging) {
+    popup.style.left = e.clientX - offsetX + 'px';
+    popup.style.top = e.clientY - offsetY + 'px';
+  }
+});*/
